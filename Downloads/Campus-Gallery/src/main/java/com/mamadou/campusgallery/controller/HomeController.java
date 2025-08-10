dc16d3ed0556791cab172263d479f4a29cf92f7d
Mamadou8Bah <mbah18791@gmail.com>
2025-08-10 15:49:44 +0000

    Implemented a new controller for testing what my backend returns

---
+++ Downloads/Campus-Gallery/src/main/java/com/mamadou/campusgallery/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.mamadou.campusgallery.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to Campus Gallery!";
+    }
+}
